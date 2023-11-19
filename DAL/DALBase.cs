1762069b6ab2e69ae81a89e2bdc3d85ec69941f6
daegrick <lucas.cabelaum@gmail.com>
2023-11-19 19:53:44 -0300

    feat: -creating DAL - moving projects to dot net 8

---
+++ DAL/DALBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALBase
+    {
+        public string getField<T>(T name) => nameof(name);
+    }
+}
---
+++ DAL/ItemPatrimonioDAL.cs
@@ -0,0 +1,21 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ItemPatrimonioDAL
+    {
+        string AllFields() =>
+            $@"{nameof(ItemPatrimonio.Ide)},
+{nameof(ItemPatrimonio.Id)},
+{nameof(ItemPatrimonio.Descricao)},
+{nameof(ItemPatrimonio.DataAquisicao)},
+{nameof(ItemPatrimonio.NumeroSerie)},
+{nameof(ItemPatrimonio.CategoriaIde)},
+{nameof(ItemPatrimonio.AlteradoEm)}";
+    }
+}
